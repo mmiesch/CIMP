a996f2e138f63f36e0c1f041a4214926ad3612f5
mmiesch <mark.s.miesch@gmail.com>
2021-12-22 10:15:46 -0700

    clean up PlotEvent

--- PlotEvent.py
+++ PlotEvent.py
@@ -6,8 +6,28 @@ import numpy as np
 from CIMP import Event as ev
 from sunpy.net import attrs as a
 
+plotcase = 2
+
+if plotcase == 1:
+    testcase = 1
+    nrgf = False
+    plotframes = (3, 6, 9, 12)
+    #scale = (700.0, 4000.0)
+    scale = (0.0, 1000.0)
+
+elif plotcase == 2:
+    testcase = 1
+    nrgf = True
+    plotframes = (3, 6, 9, 12)
+    scale = (0.0, 4.0)
+
+else:
+    print("specify a valid plotcase")
+    exit()    
+
 x = ev.event.testcase(1)
 
+# for experimenting
 #timerange = a.Time('2016/09/06 8:00:00', '2016/09/06 12:00:00')
 #x = ev.event.fromtime(a.Instrument.lasco, a.Detector.c2, timerange)
 
@@ -20,39 +40,37 @@ print(80*'-')
 # ===================
 # Optionally Apply a filter
 
-x.nrgf()
+if nrgf:
+   x.nrgf()
 
 # ===================
 import matplotlib.pyplot as plt
 import numpy as np
 import astropy.units as u
 
-fig = plt.figure(figsize=[18,8])
+fig = plt.figure(figsize=[18,10])
 
 # plot the first frame, not as a diff image
 amap = x.map(0)
 ax = fig.add_subplot(2,3,1,projection=amap)
 amap.plot(clip_interval=[10,90]*u.percent)
 
-# pick 4 frames to plot 
-p = [3,6,9,12]
+# pick 4 frames to plot
+if len(plotframes) == 0:
+    n = (x.nframes - 1)/4
+    plotframes = [n, 2*n, 3*n, x.nframes-1] 
 
 # pick a scale from one of the middle images
 ref = x.map(9)
 print(f"Reference data range: {ref.min()} to {ref.max()}")
 
-#scale = (ref.min(), ref.max())
-
-# for testcase 1 without the nrgf
-#scale = (700.0, 4000.0)
-
-# testcase 1 with the nrgf
-scale = [-0.0, 4.0]
+if len(scale) != 2:
+    scale = (ref.min(), ref.max())
 
 print(f"image scale: {scale[0]} to {scale[1]}")
 
 for i in np.arange(0,4):
-    amap = x.map(p[i])
+    amap = x.map(plotframes[i])
     ax = fig.add_subplot(2,3,i+2,projection=amap)
     amap.plot(vmin = scale[0], vmax = scale[1])
 
