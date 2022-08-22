aca8fb7948568851771f57146b03563d516b59e7
mmiesch <mark.s.miesch@gmail.com>
2022-08-22 10:48:59 -0600

    atbdnoisegate implemented

--- atbdnoisegate.py
+++ atbdnoisegate.py
@@ -3,15 +3,22 @@
 This script is specifically for making a noisegate figure for the ccor atbd
 """
 
+import os
 import matplotlib.pyplot as plt
+import noisegate as ng
 import numpy as np
-import sunpy.map
 import sunpy.visualization.colormaps as cm
 
 from astropy.io import fits
 from CIMP import Snapshot as snap
 from CIMP import Enhance
 from sunpy.net import attrs as a
+
+# for warning / error statements; print red, yellow text to terminal
+red = '\033[91m'
+yellow = '\033[93m'
+cend = '\033[0m'
+
 #------------------------------------------------------------------------------
 # parameters common to all images
 
@@ -24,13 +31,12 @@ file1 = dir+'/image068.fts'
 outdir = '/home/mark.miesch/Products/image_processing/figs/'
 
 #------------------------------------------------------------------------------
-# choose the images you want to compare
 
 fig = 1
 
 if fig == 1:
 
-    outfile = 'lasco_ng1.png'
+    outfile = 'lasco_ng_shot_f2.0.png'
 
     cmap = plt.get_cmap('soholasco2')
     #cmap = plt.get_cmap('stereocor2')
@@ -39,6 +45,10 @@ if fig == 1:
     scale1 = (0.2, 1.0)
 
     title2 = 'Noisegate filter'
+    cubesize = (12,12,12)
+    model = 'shot'
+    factor = 2.0
+    dcidx = 67
     scale2 = (0.2, 1.0)
 
 else:
@@ -46,28 +56,64 @@ else:
     exit()
 
 #------------------------------------------------------------------------------
-# First image
+# First image - before filter
 
 x1 = fits.open(file1)[0]
 
 #------------------------------------------------------------------------------
-# second image
+# load data
+
+# first pass: get list of all valid files
+files = []
+nx = None
+ny = None
+for file in os.listdir(dir):
+    fpath = dir+'/'+file
+    try:
+        assert(os.path.isfile(fpath))
+        assert("median" not in file)
+        hdu = fits.open(fpath)[0]
+        if nx is None:
+            nx = hdu.data.shape[0]
+            ny = hdu.data.shape[1]
+        else:
+            assert(hdu.data.shape[0] == nx)
+            assert(hdu.data.shape[1] == ny)
+        files.append(file)
+    except Exception as e:
+        print(red+f"{e}\nSkipping {file}"+cend)
+        pass
+
+# second pass: get data
+Nfiles = len(files)
+print(yellow+f"Number of valid files: {Nfiles}"+cend)
+dcube = np.zeros((Nfiles,nx,ny))
+idx = 0
+for file in files:
+    fpath = dir+'/'+file
+    hdu = fits.open(fpath)[0]
+    dcube[idx,:,:] = hdu.data.astype('float')
+    idx += 1
+
+print(yellow+f"Data shape: {dcube.shape}"+cend)
 
 #------------------------------------------------------------------------------
-print(f"x1 minmax: {np.min(x1.data)} {np.max(x1.data)}")
-#print(f"x2 minmax: {x2.min()} {x2.max()}")
+# apply noisegate filter
 
-print(f"x1 res: {x1.data.shape[0]} {x1.data.shape[1]}")
-#print(f"x2 res: {x2.data.shape[0]} {x2.data.shape[1]}")
+ng.noise_gate_batch(dcube, cubesize=cubesize, model=model,
+                    factor = factor)
 
+#------------------------------------------------------------------------------
 print(f"x1 time {x1.header['DATE-OBS']}")
 
 #------------------------------------------------------------------------------
 # plot
 
 data1 = x1.data
-data2 = x1.data
-#data2 = x2.data
+data2 = dcube[dcidx,:,:]
+
+print(f"x1 minmax: {np.min(data1)} {np.max(data1)}")
+print(f"x2 minmax: {np.min(data2)} {np.max(data2)}")
 
 fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(12,6))
 
