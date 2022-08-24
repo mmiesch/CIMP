1850c4bfc0f85302bca3f9bb1fabcf75568c5f1b
mmiesch <mark.s.miesch@gmail.com>
2022-08-24 10:53:45 -0600

    spiffy mask annulus

--- atbdqc.py
+++ atbdqc.py
@@ -19,8 +19,8 @@ detector1 = a.Detector.cor2
 dir1='/home/mark.miesch/sunpy/data/secchi_cor2/L1/2012/09/'
 bgfile1 = dir1+'background.fts'
 file1 = dir1+'20/20120920_153900_14c2A.fts'
-rmin = 0.16
-rmax = 1.0
+rmin1 = 0.16
+rmax1 = 1.0
 
 # L0.5 LASCO data
 instrument2 = a.Instrument.lasco
@@ -28,14 +28,37 @@ detector2 = a.Detector.c3
 dir2='/home/mark.miesch/data/lasco_monthly/c3/2012_04/'
 bgfile2 = dir2+'background.fts'
 file2 = dir2+'15/32296647.fts'
-rmin = 0.16
-rmax = 1.0
+rmin2 = 0.16
+rmax2 = 1.0
 
 outdir = '/home/mark.miesch/Products/image_processing/figs/'
 
 #------------------------------------------------------------------------------
+# spiffy version of mask_annulus for inclusion in ATBD
 
-fig = 1
+def mask_annulus_spiffy(im, rmin = 0.0, rmax = np.inf, missingval = 0.0):
+
+    # annular radii in pixels (squared)
+    nn = 0.5 * float(np.min((im.shape[0], im.shape[1])))
+    rr1 = (rmin*nn)**2
+    rr2 = (rmax*nn)**2
+
+    # center of image
+    x0 = 0.5*float(im.shape[0])
+    y0 = 0.5*float(im.shape[1])
+
+    x = np.tile(np.arange(im.shape[0]),(im.shape[1],1)).T
+    y = np.tile(np.arange(im.shape[1]),(im.shape[0],1))
+    rr = (x-x0)**2 + (y-y0)**2
+    mask = (rr <= rr1) | (rr >= rr2)
+    return np.where(mask, missingval, im)
+
+
+#------------------------------------------------------------------------------
+
+fig = 2
+
+amask = False
 
 if fig == 1:
 
@@ -45,7 +68,21 @@ if fig == 1:
     cmap2 = plt.get_cmap('soholasco2')
 
     title1 = 'Invalid pixels outside FOV'
-    scale1 = (0, 1.0e-9)
+    scale1 = (0, 4.0e-9)
+
+    title2 = 'Corrupted image'
+    scale2 = (0, 4000)
+
+elif fig == 2:
+
+    outfile = 'qc_beta.png'
+
+    cmap1 = plt.get_cmap('stereocor2')
+    cmap2 = plt.get_cmap('soholasco2')
+
+    title1 = 'Invalid pixels outside FOV'
+    scale1 = (0, 4.0e-9)
+    amask = True
 
     title2 = 'Corrupted image'
     scale2 = (0, 4000)
@@ -53,15 +90,6 @@ if fig == 1:
 else:
     print("pick a valid figure number")
     exit()
-
-#------------------------------------------------------------------------------
-# select color map
-
-try:
-    cmap1 = cmap
-    cmap2 = cmap
-except:
-    pass
 
 #------------------------------------------------------------------------------
 # First image
@@ -88,7 +116,11 @@ print(f"x2 time {x2.time}")
 #------------------------------------------------------------------------------
 # plot
 
-data1 = x1.data
+if amask:
+    data1 = mask_annulus_spiffy(x1.data, rmin = rmin1, rmax = rmax1)
+else:
+    data1 = x1.data
+
 data2 = x2.data
 
 fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(12,6))
