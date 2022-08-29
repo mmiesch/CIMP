132f2afca5a1b218739760f3e967c6202a529e3f
mmiesch <mark.s.miesch@gmail.com>
2022-08-29 17:00:22 -0600

    process single L3 file at a time

--- CIMP/L3Proc.py
+++ CIMP/L3Proc.py
@@ -14,76 +14,55 @@ red = '\033[91m'
 yellow = '\033[93m'
 cend = '\033[0m'
 
-class l3proc_ng:
+class l3proc:
     """
     Class for CCOR L3 data processing with noise-gate.  The use of noise-gate filtering requires the analysis of N image
     """
 
-    def __init__(self, infile, outdir, Nimages = 18, rmin = 0.0, rmax = np.inf):
+    def __init__(self, infile, outdir):
         """
         infile: This is intended to represent a new L1b (CCOR-1) or L2 (CCOR-2) input file that has been created as part of a real-time operational pipeline.
 
         outdir: The output directory where the L3 data should be written
 
-        Nimages: the number of images in the image sequence.  This is used by the noise-gate filter to estimate the noise spectrum.
-
         rmin, rmax: FOV (normalized for minimum extent of image axes) for mask_annulus
 
         """
 
-        self.infile = os.path.basename(infile)
-        self.Nimages = Nimages
+        self.infile = infile
 
-        # parent directory containing L1b (CCOR-1) or L2 (CCOR-1) image files
-        self.indir = os.path.dirname(infile)
-        self.outdir = outdir
-        assert(self.indir != self.outdir)
+        # generate output filename
+        filename = os.path.basename(infile).split('_')
+        filename.insert(1,'L3')
+        self.outfile = outdir+'/'+'_'.join(filename)
 
-        # loop over files to obtain the most recent N files that pass the
-        # QC filter.  In the actual operational implementation this would look
-        # different.  However, for now, take advantange of my standard naming
-        # convention for L2 proxy data.  In this convention, the filename is
-        # includes the time stamp in such a way that an alphabetic ordering 
-        # of the files will also be a chronological ordering.  So, start with
-        # the last file in an alphabetical ordering and work backwards until
-        # you get to N=18
 
-        flist = list(sorted(os.listdir(self.indir)))
+    def process(self, rmin = 0.0, rmax = np.inf):
+        # Basic L3 pipeline
 
-        try:
-            idx = flist.index(self.infile)
-        except ValueError:
-            print(red+"ERROR: infile not found"+cend)
-            exit()
+        hdu = fits.open(self.infile)
+        indata = hdu[0].data
+        self.header = hdu[0].header
 
-        # get resolution from first file
-        hdu = fits.open(self.indir+'/'+flist[idx])
-        self.Nx, self.Ny = hdu[0].data.shape
+        # mask annulus
+        Enhance.mask_annulus(indata, rmin = rmin, rmax = rmax)
+
+        # median downsample
+        self.data = Enhance.downsample(indata)
+        self.nx, self.ny = self.data.shape
+        self.header['NAXIS1'] = self.nx
+        self.header['NAXIS2'] = self.ny
+        self.header['CRPIX1'] /= 2
+        self.header['CRPIX2'] /= 2
+        self.header['CDELT1'] *= 2
+        self.header['CDELT2'] *= 2
+
         hdu.close()
 
-        # resolution of downsampled image
-        self.nx, self.ny = int(self.Nx/2), int(self.Ny/2)
+    def write(self):
 
-        # mask annulus, downsample, and QC check during the initial read
-
-        self.files = []
-        self.headers = []
-        self.images = np.zeros((self.Nimages, self.nx, self.ny), dtype='float32')
-        while len(self.files) < self.Nimages:
-            hdu = fits.open(self.indir+'/'+flist[idx])
-            data = hdu[0].data
-            header = hdu[0].header
-            Enhance.mask_annulus(data, rmin = rmin, rmax = rmax)
-            data = Enhance.downsample(data)
-            assert(data.shape == (self.nx, self.ny))
-            header['NAXIS1'] = self.nx
-            header['NAXIS2'] = self.ny
-            header['CRPIX1'] /= 2
-            header['CRPIX2'] /= 2
-            header['CDELT1'] *= 2
-            header['CDELT2'] *= 2
-            self.images[len(self.files),:,:] = data
-            self.files.append(flist[idx])
-            self.headers.append(hdu[0].header)
-            hdu.close()
-            idx -= 1
+        assert(self.outfile != self.infile)
+        hdu_out = fits.PrimaryHDU(self.data, self.header)
+        hdulist = fits.HDUList([hdu_out])
+        hdulist.writeto(self.outfile, overwrite = True)
+        hdulist.close()
--- l3run.py
+++ l3run.py
@@ -14,34 +14,40 @@ dir = '/home/mark.miesch/Products/image_processing/ATBD/data/'
 rmin = 0.16
 rmax = 1.0
 
+# if Nfiles list is None, do all files in directory
+Nfiles = None
+
 fig = 1
 
 if fig == 1:
 
     # L0.5 LASCO data
-    infile = dir+'/lasco_c3/L2proxy_2012_04/LASCOC3_2012_04_15_063005.fts'
+    Nfiles = 10
+    endfile = dir+'/lasco_c3/L2proxy_2012_04/LASCOC3_2012_04_15_063005.fts'
     outdir = dir+'/lasco_c3/L3_2012_04'
 
 elif fig == 2:
 
     # L0.5 LASCO data
-    infile = dir+'lasco_c3/L2proxy_2014_01/LASCOC3_2014_01_15_125405.fts'
+    Nfiles = 10
+    endfile = dir+'lasco_c3/L2proxy_2014_01/LASCOC3_2014_01_15_125405.fts'
     outdir = dir+'lasco_c3/L3_2014_01'
 
 elif fig == 3:
 
     # L1 STEREO-A data
-    infile = dir+'/stereo_a/L2proxy_2012_09/STEREOA_2012_09_15_183900.fts'
+    Nfiles = 10
+    endfile = dir+'/stereo_a/L2proxy_2012_09/STEREOA_2012_09_15_183900.fts'
     outdir = dir+'/stereo_a/L3_2012_09'
 
 elif fig == 4:
 
     # HAO CME model
-    infile = dir+'/model/CME0_pos30/L2proxy/Model0_2010_04_16_021057.fts'
+    Nfiles = 10
+    endfile = dir+'/model/CME0_pos30/L2proxy/Model0_2010_04_16_021057.fts'
     outdir = dir+'/model/CME0_pos30/L3'
     rmin = 0.0
     rmax = np.inf
-
 
 else:
     print("pick a valid figure number")
@@ -51,8 +57,31 @@ if not os.path.exists(outdir):
     os.mkdir(outdir)
 
 #------------------------------------------------------------------------------
-#  Use the background file as a reference for the correct resolution
+# get a list of files of length Nfiles, ending at endfile
 
-x = proc.l3proc_ng(infile, outdir)
+indir = os.path.dirname(endfile)
+dirlist = list(sorted(os.listdir(indir)))
+
+if Nfiles == None:
+    flist = dirlist
+else:
+
+    try:
+        i2 = dirlist.index(os.path.basename(endfile))
+    except ValueError:
+        print("ERROR: file not found")
+        exit()
+
+    i1 = np.max((0,int(i2 - Nfiles + 1)))
+
+    flist = dirlist[i1:i2+1]
+
+#------------------------------------------------------------------------------
+
+for file in flist:
+    fpath = indir+'/'+file
+    x = proc.l3proc(fpath, outdir)
+    x.process(rmin = rmin, rmax = rmax)
+    x.write()
 
 #------------------------------------------------------------------------------
