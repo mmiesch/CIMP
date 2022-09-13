59e3aae1e86f1d2862fded3d1f6f14a895fc6fef
mmiesch <mark.s.miesch@gmail.com>
2022-09-13 09:10:21 -0600

    add ngcompare.py

--- ngcompare.py
+++ ngcompare.py
@@ -1,6 +1,6 @@
 
 """
-Script to create movies showing images before and after processing
+Script to compare different ng methods for the ATBD
 """
 
 import datetime
@@ -28,7 +28,8 @@ def get_time(header, source):
         return 0
 
 #------------------------------------------------------------------------------
-def noisegate(images, cubesize=(12,12,12), factor = 4.0):
+def noisegate(images, cubesize=(12,12,12), factor = 4.0, model = 'constant', \
+              dkfactor = 4.0):
 
     nap = int((2*cubesize[0])/3)
     nt, nx, ny = images.shape
@@ -38,103 +39,70 @@ def noisegate(images, cubesize=(12,12,12), factor = 4.0):
 
     print(f"Applying noisegate {images.shape} {dcube.shape}")
 
-    dcubeng = ng.noise_gate_batch(dcube, cubesize=cubesize, model='constant',
-                factor = factor)
+    if model == 'hybrid':
+        dcubeng = ng.noise_gate_batch(dcube, cubesize=cubesize, model=model,
+                    factor = factor, dkfactor = dkfactor)
+    else:
+        dcubeng = ng.noise_gate_batch(dcube, cubesize=cubesize, model=model,
+            factor = factor)
+
     return dcubeng[nap:-nap,:,:]
 
 #------------------------------------------------------------------------------
-fig = 5
+fig = 2
 
 rootdir = '/home/mark.miesch/Products/image_processing/ATBD'
-ngflag = True
 framedir = None
-factor = 4.0
 
 if fig == 1:
     source = 'lasco'
-    dir2 = rootdir + '/data/lasco_c3/L3_2014_01'
-    cmap2 = plt.get_cmap('soholasco2')
-    endfile = 'LASCOC3_L3_2014_01_17_053005.fts'
+    dir1 = rootdir + '/data/lasco_c3/L3_2021_05'
+    dir2 = dir1
+    cmap1 = plt.get_cmap('soholasco2')
+    cmap2 = cmap1
+    endfile = 'LASCOC3_L3_2021_05_23_223007.fts'
     duration = 2.0
-    scale2 = (0.0, 0.6)
-    factor = 10.0
 
-    dir1 = rootdir + '/data/lasco_c3/L2proxy_2014_01'
-    cmap1 = plt.get_cmap('stereocor2')
-    scale1 = (1.0, 1.2)
+    model1 = 'hybrid'
+    cubesize1=(12,12,12)
+    factor1 = 4.0
+    dkfactor1 = 6.0
+    scale1 = (0.0, 0.6)
+
+    model2 = 'constant'
+    cubesize2=(12,12,12)
+    factor2 = 6.0
+    dkfactor2 = 6.0
+    scale2 = (0.0, 0.6)
 
     pdir = '/home/mark.miesch/Products/image_processing'
-    outfile = rootdir+'/movies/lasco_2014_01_16_ba.mp4'
-    #framedir = pdir+f'/frames/2021_05_16_ba'
+    outfile = rootdir+'/movies/lasco_2021_05_ng_ch.mp4'
+    framedir = pdir+f'/frames/2021_05_ng_ch'
 
 elif fig == 2:
-    source = 'stereo'
-    dir2 = rootdir + '/data/stereo_a/L3_2012_09'
-    cmap2 = plt.get_cmap('soholasco2')
-    endfile = 'STEREOA_L3_2012_09_17_022400.fts'
-    duration = 2.0
-    scale2 = (0.0, 1.0)
-
-    dir1 = rootdir + '/data/stereo_a/L2proxy_2012_09'
-    cmap1 = plt.get_cmap('stereocor2')
-    scale1 = (1.0, 1.4)
-
-    pdir = '/home/mark.miesch/Products/image_processing'
-    outfile = rootdir+'/movies/stereo_2012_09_16_ba.mp4'
-    #framedir = pdir+f'/frames/2012_09_16_ba'
-
-elif fig == 3:
-    source = 'lascoL1'
-    dir2 = rootdir + '/data/lasco_c3/L3_L1_2014_01'
-    cmap2 = plt.get_cmap('soholasco2')
-    endfile = 'LASCOC3_L3_L1_2014_01_17_050449.fts'
-    duration = 2.0
-    scale2 = (0.0, 0.6)
-    factor = 6.0
-
-    dir1 = rootdir + '/data/lasco_c3/L2proxy1_2014_01'
-    cmap1 = plt.get_cmap('stereocor2')
-    scale1 = (1.0, 1.2)
-
-    pdir = '/home/mark.miesch/Products/image_processing'
-    outfile = rootdir+'/movies/lascoL1_2014_01_16_ba.mp4'
-    #framedir = pdir+f'/frames/2021_05_16_ba'
-
-elif fig == 4:
     source = 'lasco'
-    dir2 = rootdir + '/data/lasco_c3/L3_2021_05'
-    cmap2 = plt.get_cmap('soholasco2')
+    dir1 = rootdir + '/data/lasco_c3/L3_2021_05'
+    dir2 = dir1
+    cmap1 = plt.get_cmap('soholasco2')
+    cmap2 = cmap1
     endfile = 'LASCOC3_L3_2021_05_23_223007.fts'
     duration = 2.0
-    scale2 = (0.0, 0.6)
-    factor = 6.0
 
-    dir1 = rootdir + '/data/lasco_c3/L2proxy_2021_05'
-    cmap1 = plt.get_cmap('stereocor2')
-    scale1 = (1.0, 1.3)
+    model1 = 'constant'
+    cubesize1=(12,12,12)
+    factor1 = 4.0
+    dkfactor1 = 6.0
+    scale1 = (0.0, 0.6)
+
+    model2 = 'constant'
+    cubesize2=(12,12,12)
+    factor2 = 6.0
+    dkfactor2 = 6.0
+    scale2 = (0.0, 0.6)
 
     pdir = '/home/mark.miesch/Products/image_processing'
-    outfile = rootdir+'/movies/lasco_2021_05_23_ba.mp4'
-    framedir = pdir+f'/frames/2021_05_23_f6'
-
-elif fig == 5:
-    # experimenting with 2021 data
-    source = 'lasco'
-    dir2 = rootdir + '/data/lasco_c3/L3_2021_05'
-    cmap2 = plt.get_cmap('soholasco2')
-    endfile = 'LASCOC3_L3_2021_05_23_223007.fts'
-    duration = 2.0
-    scale2 = (0.0, 0.6)
-    factor = 10.0
-    ngflag = False
-
-    dir1 = rootdir + '/data/lasco_c3/L2proxy_2021_05'
-    cmap1 = plt.get_cmap('stereocor2')
-    scale1 = (1.0, 1.3)
-
-    pdir = '/home/mark.miesch/Products/image_processing'
-    outfile = rootdir+'/movies/lasco_2021_05_23_nong.mp4'
-    framedir = pdir+f'/frames/2021_05_23_nong'
+    outfile = rootdir+'/movies/lasco_2021_05_ng_46.mp4'
+    framedir = pdir+f'/frames/2021_05_ng_46'
 
 else:
     print("pick a valid figure number")
@@ -172,30 +140,22 @@ while (dt <= dtmax) and (idx < len(flist)):
 
 Nfiles = len(files)
 
-hdu2 = fits.open(dir2+'/'+files[0])
-nx2, ny2 = hdu2[0].data.shape
-hdu2.close()
+hdu = fits.open(dir2+'/'+files[0])
+nx1, ny1 = hdu[0].data.shape
+nx2, ny2 = nx1, ny1
+hdu.close()
 
-file1 = files[0].replace('_L3','')
-hdu1 = fits.open(dir1+'/'+file1)
-nx1, ny1 = hdu1[0].data.shape
-hdu1.close()
-
-images1 = np.zeros((Nfiles,nx1,ny1), dtype = 'float')
-images2 = np.zeros((Nfiles,nx2,ny2), dtype = 'float')
+images = np.zeros((Nfiles,nx1,ny1), dtype = 'float')
 for idx in np.arange(Nfiles):
    hdu2 = fits.open(dir2+'/'+files[idx])
-   images2[Nfiles-1-idx,:,:] = hdu2[0].data
+   images[Nfiles-1-idx,:,:] = hdu2[0].data
    hdu2.close()
-   file1 = files[idx].replace('_L3','')
-   hdu1 = fits.open(dir1+'/'+file1)
-   images1[Nfiles-1-idx,:,:] = hdu1[0].data
-   hdu1.close()
 
-if ngflag:
-    images2 = noisegate(images2, factor = factor)
-    nt = 8
-    images1 = images1[nt:,:,:]
+images1 = noisegate(images, cubesize=cubesize1, factor = factor1, \
+                    model=model1, dkfactor=dkfactor1)
+
+images2 = noisegate(images, cubesize=cubesize2, factor = factor2, \
+                    model=model2, dkfactor=dkfactor2)
 
 #------------------------------------------------------------------------------
 
